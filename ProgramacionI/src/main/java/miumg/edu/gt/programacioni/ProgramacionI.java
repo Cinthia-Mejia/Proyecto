d57fda977a8850ddc0509af04666701e45768896
Cinthia-Mejia <cmejiao2@miumg.edu.gt>
2024-03-18 21:57:29 -0600

    initial commit

---
+++ ProgramacionI/src/main/java/miumg/edu/gt/programacioni/ProgramacionI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package miumg.edu.gt.programacioni;
+
+/**
+ *
+ * @author sergi
+ */
+public class ProgramacionI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
