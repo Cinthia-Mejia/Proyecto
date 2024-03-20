6f8e4be56a3f0fba17e206684f5238693eb196a5
Cinthia-Mejia <cmejiao2@miumg.edu.gt>
2024-03-19 22:51:45 -0600

    Initial commit

---
+++ ProgramaIGrupo62024/src/main/java/miumg/edu/gt/programaigrupo62024/ProgramaIGrupo62024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package miumg.edu.gt.programaigrupo62024;
+
+/**
+ *
+ * @author sergi
+ */
+public class ProgramaIGrupo62024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
