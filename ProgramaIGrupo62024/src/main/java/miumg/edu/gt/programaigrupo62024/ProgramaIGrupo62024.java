d80ecea9cf3d1cd7088542f4d26afa17235cd45f
MIGUEL <MIGUEL@Migu3l>
2024-03-20 22:17:30 -0600

    Prueba Repositorio

--- ProgramaIGrupo62024/src/main/java/miumg/edu/gt/programaigrupo62024/ProgramaIGrupo62024.java
+++ ProgramaIGrupo62024/src/main/java/miumg/edu/gt/programaigrupo62024/ProgramaIGrupo62024.java
@@ -12,5 +12,6 @@ public class ProgramaIGrupo62024 {
 
     public static void main(String[] args) {
         System.out.println("Hello Compañeros");
-    }
+        System.out.println("Prueba numero 1"); 
+    }    
 }
