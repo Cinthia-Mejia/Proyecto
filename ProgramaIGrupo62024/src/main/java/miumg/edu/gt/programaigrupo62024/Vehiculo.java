2b365db1995b0c05240638b089fa7eb21de1e5ef
Cinthia-Mejia <cmejiao2@miumg.edu.gt>
2024-03-23 12:29:16 -0600

    Creacion de Clases

---
+++ ProgramaIGrupo62024/src/main/java/miumg/edu/gt/programaigrupo62024/Vehiculo.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sergi
+ */
+    
+package miumg.edu.gt.programaigrupo62024;
+
+    public class Vehiculo {
+        String color;
+        String marca;
+        String modelo;
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+      
+}
+
